c80c2a34675fd33fd91319e547c6802b709b72a8
Dennis <dennis.simas@gmail.com>
2023-09-14 19:24:07 +0100

    feat: function shuffleArray

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,25 @@
 const main = document.getElementById("main");
-
+renderCard();
 function renderCard() {
-  for (let index = 0; index < 8; index++) {
-    let carta = document.createElement("img");
-    carta.src = "./src/img/QuestionBlock.png";
-    main.appendChild(carta);
+  const arrDuplicado = [...personagens, ...personagens];
+
+  const shufflePersonagens = shuffleArray(personagens);
+
+  for (let index = 0; index < personagens.length; index++) {
+    let cartaA = document.createElement("img");
+    cartaA.src = "./src/img/QuestionBlock.png";
+    let cartaB = document.createElement("img");
+    cartaB.src = "./src/img/QuestionBlock.png";
+
+    main.appendChild(cartaA);
+    main.appendChild(cartaB);
   }
 }
 
-renderCard();
+function shuffleArray(arr) {
+  for (let index = arr.length - 1; index > 0; index--) {
+    const j = Math.floor(Math.random() * (index + 1));
+    [arr[index], arr[j]] = [arr[j], arr[index]];
+  }
+  return arr;
+}
